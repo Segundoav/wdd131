39b073cf2a28f4ccf6136bea804058435f929c32
segundoav <segundoav@hotmail.com>
2025-05-20 17:33:39 -0500

    last change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,10 @@
-// Obtiene el año actual y lo inserta en el span con id="currentyear"
-const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
+// Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtiene el año actual y lo inserta en el span con id="currentyear"
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
 
-// Obtiene la fecha de la última modificación del documento
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+    // Obtiene la fecha de la última modificación del documento
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Última modificación: ${lastModified}`;
+});
